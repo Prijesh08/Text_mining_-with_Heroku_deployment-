0338ad215ab996d1115a0e6df12770336d08fab7
Prijesh <prijeshdhola125@gmail.com>
2021-07-24 01:16:44 +0530

    its completed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 # import pickle
 import spacy
+import click._bashcomplete
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from wordcloud import STOPWORDS
